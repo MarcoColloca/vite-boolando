224ff7d9f4f9dd2d3ac1491c77ee7035578acd68
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-11 14:44:59 +0200

    Fix → Moved CardBooland component into separate folder

---
+++ src/components/single-components/CardBoolando.vue
@@ -0,0 +1,40 @@
+<script>
+    export default{
+        data(){
+            return{
+                test: 'test',
+            }
+        }
+    }
+</script>
+
+
+
+<template>
+    <div class="col-4 card">
+        <div class="card__img">
+            <img src="/img/1.webp" alt="">
+            <span class="badge-discount">-50%</span>
+            <span class="badge-type">Sostenibilità</span>
+            <span class="box-heart">&hearts;</span>
+            <div class="overlay">
+                <img src="/img/1b.webp" alt="">
+            </div>
+        </div>
+
+        <div class="card__description">
+            <span class="card__description__brand">Levi's</span>
+            <span class="card__description__item">RELAXED FIT TEE UNISEX</span>
+            <span class="card__description__price">14.99 &euro;</span> 
+            <span class="card__description__old-price">29.99 &euro;</span> 
+        </div>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+    .test{
+        color: crimson;
+    }
+</style>
