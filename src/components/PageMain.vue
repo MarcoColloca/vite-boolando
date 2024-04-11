4b37b558bd8ae87ab717fb89dd94f5d63c82a140
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-11 14:56:44 +0200

    Fix → moved card style into CardBoolando component

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -116,6 +116,12 @@
         components:{
             CardBoolando,
         },
+
+        methods:{
+            hideCardImg(){
+                console.log('ciao')
+            }
+        },
     }
 </script>
 
@@ -125,7 +131,9 @@
     <main class="page-main">
         <div class="container">
             <div class="row page-main__box-card">                
-                <CardBoolando></CardBoolando>
+                <CardBoolando
+                 v-for="(card,i) in products"
+                ></CardBoolando>
             </div>
         </div>
     </main>
@@ -133,97 +141,9 @@
 
 
 <style lang="scss" scoped>
-    @use '../style/partials/variables' as *;
 
     .page-main{
         padding: 40px 0px;
     }
 
-    .card{
-        position: relative;
-        margin-bottom: 20px;
-        &:hover{
-            .overlay{
-                opacity: 1;
-            }
-        }
-    }
-
-    .card__img{
-        position: relative;
-        display: flex;
-    }
-
-    .overlay {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        display: block;
-        justify-content: center;
-        align-items: center;
-        opacity: 0;
-        z-index: 1;
-    }
-
-    .badge-discount{
-        background-color: $color_red;
-    }
-
-    .badge-type{
-        background-color: $color_green;
-        left: 50px;
-    }
-
-    .box-heart{
-        position: absolute;
-        top: 10px;
-        right: 0px;
-        font-size: 35px;
-        color: black;
-        background-color: white;
-        z-index: 99;
-        padding: 0px 15px;
-        cursor: pointer;
-        &:hover{
-            color: $color_red;
-        }
-    }
-
-    .card__description__brand, 
-    .card__description__item{
-        display: block;
-    }
-
-    .card__description__brand{
-        font-size: 15px;
-    }
-
-    .card__description__item{
-        font-weight: bold;
-        font-size: 17px;
-    }
-
-    .card__description__price{
-        color: $color_red;
-        font-weight: bold;
-        font-size: 16px;
-        margin-right: 10px;
-    }
-
-    .card__description__old-price{
-        text-decoration: line-through;
-    }
-
-    [class^="badge"]{
-        padding: 5px;
-        color: white;
-        position: absolute;
-        bottom: 50px;
-        z-index: 99;
-    }
-
-    .badge-type.badge-alone,
-    .badge-discount.badge-alone{
-        left: 0;
-    }
 </style>
--- src/components/single-components/CardBoolando.vue
+++ src/components/single-components/CardBoolando.vue
@@ -1,9 +1,15 @@
 <script>
     export default{
+
+
+        
         data(){
             return{
                 test: 'test',
             }
+        },
+
+        mounted(){
         }
     }
 </script>
@@ -34,7 +40,95 @@
 
 
 <style lang="scss" scoped>
-    .test{
-        color: crimson;
+    @use '../../style/partials/variables' as *;
+
+
+    .card{
+        position: relative;
+        margin-bottom: 20px;
+        &:hover{
+            .overlay{
+                opacity: 1;
+            }
+        }
     }
+
+    .card__img{
+        position: relative;
+        display: flex;
+    }
+
+    .overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: block;
+        justify-content: center;
+        align-items: center;
+        opacity: 0;
+        z-index: 1;
+    }
+
+    .badge-discount{
+        background-color: $color_red;
+    }
+
+    .badge-type{
+        background-color: $color_green;
+        left: 50px;
+    }
+
+    .box-heart{
+        position: absolute;
+        top: 10px;
+        right: 0px;
+        font-size: 35px;
+        color: black;
+        background-color: white;
+        z-index: 99;
+        padding: 0px 15px;
+        cursor: pointer;
+        &:hover{
+            color: $color_red;
+        }
+    }
+
+    .card__description__brand, 
+    .card__description__item{
+        display: block;
+    }
+
+    .card__description__brand{
+        font-size: 15px;
+    }
+
+    .card__description__item{
+        font-weight: bold;
+        font-size: 17px;
+    }
+
+    .card__description__price{
+        color: $color_red;
+        font-weight: bold;
+        font-size: 16px;
+        margin-right: 10px;
+    }
+
+    .card__description__old-price{
+        text-decoration: line-through;
+    }
+
+    [class^="badge"]{
+        padding: 5px;
+        color: white;
+        position: absolute;
+        bottom: 50px;
+        z-index: 99;
+    }
+
+    .badge-type.badge-alone,
+    .badge-discount.badge-alone{
+        left: 0;
+    }
+
 </style>
